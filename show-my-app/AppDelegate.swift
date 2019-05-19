5ece9abe621f6b579134dd7bf45158531c6ec912
jfcontart <jfcontart@idemobi.com>
2019-05-19 15:18:01 +0200

    [INIT]

---
+++ show-my-app/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  show-my-app
+//
+//  Created by JFC idéMobi on 19/05/2019.
+//  Copyright © 2019 idéMobi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ show-my-app/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  show-my-app
+//
+//  Created by JFC idéMobi on 19/05/2019.
+//  Copyright © 2019 idéMobi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
